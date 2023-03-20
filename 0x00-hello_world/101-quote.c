9842e0d4ef53135ee112bdc3c74f8d196beffffd
BewelltecGitHub <bewell.gh@gmail.com>
2023-03-20 15:26:12 +0000

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - A program which output a line of standard error
- * Always 1  (sucesful)
+ * Return 1  (success)
  */
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" stdout);
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
