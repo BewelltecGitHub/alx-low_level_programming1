cb28e490a3edd3d49f2565a087078b239bc9dfe4
BewelltecGitHub <bewell.gh@gmail.com>
2023-03-20 11:12:32 +0000

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio>
+/**
+ * main - My first program that displays a line using printf function and return zero.
+ * Return: 0 (means successful)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art\n");
+return (0);
+}
