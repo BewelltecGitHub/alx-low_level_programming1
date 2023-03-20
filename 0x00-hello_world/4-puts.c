9e2e14b257ac2aef0889376145f778104a6a3601
BewelltecGitHub <bewell.gh@gmail.com>
2023-03-20 11:01:09 +0000

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -  My first program that displays a line using the puts function
+ * Return: 0 (means successful)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
