6f9404e3adcaf04e876b63c2cb9a763df5e76179
BEWELLTECNET <bewell.gh@gmail.com>
2023-03-22 22:08:02 -0700

    0-putchar.c

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef main_ h 
+#define main_ h
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c)
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack bauer(void);
+void times_ table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_-times_table(int n);
